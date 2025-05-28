192bff222702c13866ae2f214671042b0a6ba88b
NickyBenz <nikhil.bezwada07@gmail.com>
2025-05-29 00:43:29 +0800

    Stage 1

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 
 ## Overview
-Crypto Legacy Bond is an innovative blockchain-based savings vehicle designed as a sentimental gift for our co-founder’s newborn daughter. This unique product recognizes the absence of similar offerings in the market and aims to provide a secure and meaningful way to save cryptocurrencies.
+Crypto Legacy Bond is an innovative blockchain-based savings vehicle. This unique product recognizes the absence of similar offerings in the market and aims to provide a secure and meaningful way to save cryptocurrencies.
 
 ## Features
 Time-Locked Smart Contracts: Users can lock cryptocurrencies (WBTC, ETH, USDC, USDT) in a smart contract on Base L2 (Ethereum layer-2) until a customizable maturity date. Withdrawals or sales are prohibited before maturity, ensuring the funds are preserved for their intended purpose.
+
+## Contract Functions
+
+- makeDeposit: This function takes a balance and maturity from the user for their tokens to create and store a holding 
+- withDraw: If the holding is past maturity, the user is allowed to withdraw from their holding
+- _getUSDAmount(): Internal function accessing the WBTC/USDT pool to check the USD value of the pool, ensuring it works.
+
--- src/DeBond.sol
+++ src/DeBond.sol
@@ -1,15 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+event HoldingCreated(address, uint256, uint256); //Event to emit whenever a new user creates a bond holding
+event SavingsWithdrawed(address, uint256); //Event to emit whenever a user successfully withdraws from their hoding
+
+error CannotWithdrawBeforeMaturity(uint256 maturity); //Error to revert whenever a user attempts to withdraw from their bond before their maturity date
+error ExceededMaxUSDAmount(uint256 amount); //Error to emit when the user attempts to deposit above the max withdrawal rate
+
+
+import {ERC20,IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IUniswapV3Pool} from "@uniswapv3/contracts/interfaces/IUniswapV3Pool.sol";
 
 contract DeBond {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    address constant  WBTCUSDCPOOL = 0xfBB6Eed8e7aa03B138556eeDaF5D271A5E1e43ef;
+    address constant cbBTC = 0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf;
+    address constant USDC = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+    uint256 constant MAXDEPOSITAMOUNT = 1000;
+    //Defined a custom struct to manage each user's holdings
+    struct Holdings{
+        uint256 balance;
+        uint256 maturity;
     }
 
-    function increment() public {
-        number++;
+    //Maps each user address to their holdings struct
+    mapping(address => Holdings) s_holdings;
+
+    //Mapping to check if the user has an active holding
+    mapping(address => bool) s_isActive;
+
+    
+    function _getUSDAmount(uint256 btc_amount) internal view returns (uint256 usdAmt){
+        (uint160 sqrtPriceX96,,,,,, ) = IUniswapV3Pool(WBTCUSDCPOOL).slot0(); //Retrieves the current spot price from the USDC/cbBTC pool on UniswapV3
+        //Convert the price into a readable price 
+        //Price = sqrtPriceX96^2 /(2^192)
+        uint256 formatted_price = (sqrtPriceX96 * sqrtPriceX96)/(2**192); 
+        //Retrieve the decimals from the USDC token contract
+        uint8 usdDecimals = ERC20(USDC).decimals(); 
+
+        usdAmt = formatted_price * (10 ** uint256(usdDecimals));
+
     }
+
 }
