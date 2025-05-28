3257ef7e1606267feb4d4cbe355c2891dfe3f70e
NickyBenz <nikhil.bezwada07@gmail.com>
2025-05-28 23:11:34 +0800

    ERC20 Imports

--- src/DeBond.sol
+++ src/DeBond.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Counter {
+contract DeBond {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
